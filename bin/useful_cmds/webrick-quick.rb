2e20c34eba32aa868aeb324dd8784d20872519b2
Asfand Qazi <ayqazi@gmail.com>
2017-04-19 16:21:10 +0100

    Add bin directory

---
+++ bin/useful_cmds/webrick-quick.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+require 'webrick'
+
+dir = Dir::pwd
+port = 6000 # + (dir.hash % 1000)
+puts "URL: http://#{Socket.gethostname}:#{port}"
+
+server = WEBrick::HTTPServer.new :Port => port, :DocumentRoot => dir
+trap("INT"){ server.shutdown }
+server.start
