7b6d38cecf58b395279d7b80a702e6301fedb0ac
Asfand Qazi <ayqazi@gmail.com>
2022-05-25 10:35:13 +0100

    update

--- bin/useful_cmds/webrick-quick.rb
+++ bin/useful_cmds/webrick-quick.rb
@@ -3,7 +3,7 @@
 require 'webrick'
 
 dir = Dir::pwd
-port = 6000 # + (dir.hash % 1000)
+port = 14000 # + (dir.hash % 1000)
 puts "URL: http://#{Socket.gethostname}:#{port}"
 
 server = WEBrick::HTTPServer.new :BindAddress => '0.0.0.0', :Port => port, :DocumentRoot => dir
