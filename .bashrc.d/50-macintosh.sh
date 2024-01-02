5e86c69444e802240f59c936282c55c74062b160
Asfand Qazi <asfand.qazi@b4bpayments.com>
2024-01-02 17:26:17 +0000

    Update mac brew setup

--- .bashrc.d/50-macintosh.sh
+++ .bashrc.d/50-macintosh.sh
@@ -1,11 +1,7 @@
 export BASH_SILENCE_DEPRECATION_WARNING=1
 
-export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
-export HOMEBREW_REPOSITORY="/opt/homebrew";
-export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
-export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
-export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-if [ -f /opt/homebrew/etc/profile.d/bash_completion.sh ] ; then
-    . /opt/homebrew/etc/profile.d/bash_completion.sh
+if [ -f /opt/homebrew/etc/bash_completion.d/brew ] ; then
+    . /opt/homebrew/etc/bash_completion.d/brew
 fi
