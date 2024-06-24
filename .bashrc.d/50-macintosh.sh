627d55454b105712cb288bfc1070a5cb1abb1445
Asfand Qazi <asfand.qazi@b4bpayments.com>
2024-06-24 11:11:05 +0100

    update

--- .bashrc.d/50-macintosh.sh
+++ .bashrc.d/50-macintosh.sh
@@ -3,3 +3,5 @@ export BASH_SILENCE_DEPRECATION_WARNING=1
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 [[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"
+
+export SSH_AUTH_SOCK="${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
