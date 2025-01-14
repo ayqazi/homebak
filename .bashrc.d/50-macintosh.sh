34c607786202ffc939cf7a91753e87dc42643f09
Asfand Qazi <ayqazi@gmail.com>
2025-01-14 17:42:33 +0000

    homebrew and asdf setup for mac

--- .bashrc.d/50-macintosh.sh
+++ .bashrc.d/50-macintosh.sh
@@ -2,6 +2,22 @@ export BASH_SILENCE_DEPRECATION_WARNING=1
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-[[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"
+HOMEBREW_PREFIX="$(brew --prefix)"
+if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
+then
+  source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+else
+  for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+  do
+    [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+  done
+fi
 
 export SSH_AUTH_SOCK="${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
+# Added by Toolbox App
+export PATH="$PATH:/Users/asfand/Library/Application Support/JetBrains/Toolbox/scripts"
+
+if [ -f "/opt/homebrew/opt/asdf/libexec/asdf.sh" ] ; then
+	. /opt/homebrew/opt/asdf/libexec/asdf.sh
+fi
--- .bashrc.d/60-asdf.sh
+++
@@ -1,3 +0,0 @@
-if [ -f "${HOME}/.asdf/asdf.sh" ] ; then
-	. /opt/homebrew/opt/asdf/libexec/asdf.sh
-fi
